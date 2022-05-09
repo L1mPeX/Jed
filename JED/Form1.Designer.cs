6840b9530ad6f4eb280ecf0af347ec984d0f1c68
itthingsyt <maxbeling@gmail.com>
2022-05-09 18:58:37 +0300

    Добавьте файлы проекта.

---
+++ JED/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace JED
+{
+    partial class Jed
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jed));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(4, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(229, 337);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Загрузка данных...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Загрузка данных...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Оперативная память (ОЗУ)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Загрузка данных...            ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Центральный процессор (ЦП)";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(624, 102);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(634, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(164, 102);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(239, 111);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(559, 337);
+            this.panel4.TabIndex = 3;
+            // 
+            // Jed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 453);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Jed";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JED";
+            this.Load += new System.EventHandler(this.Jed_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ JED/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JED
+{
+    public partial class Jed : Form
+    {
+        
+        public Jed()
+        {
+            InitializeComponent();
+        }
+          
+        public string getCPUCounter()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            dynamic firstValue = cpuCounter.NextValue();
+            Thread.Sleep(1000); 
+            dynamic secondValue = cpuCounter.NextValue();
+            return "Нагрузка: " + (int)secondValue + "%";
+
+        }
+        public string getRAMCounter()
+        {
+            double A_RAM = Math.Round(new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / 1073741824.0, 2);
+            ulong T_RAM = ((new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1073741824) + 1);
+            return "Используется: " + (T_RAM - A_RAM) + "ГБ / " + T_RAM + "ГБ";
+        }
+        public string getDriveInfo()
+        {
+            var drives = DriveInfo.GetDrives();
+            string fin_res = "";
+            foreach (DriveInfo info in drives)
+            {
+                fin_res+="Накопитель " + info.Name + "\nСвободно: " + info.TotalFreeSpace/1073741824 + "ГБ / " + info.TotalSize/1073741824 + " ГБ\nФормат диска: " + info.DriveFormat + "\n\n";
+            }
+            return fin_res;
+        }
+
+        private async void Jed_Load(object sender, EventArgs e)
+        {
+            await PC_DATA();
+        }
+        private async Task PC_DATA()
+        {
+           for (; ; )
+           {
+                label2.Text = await Task.Run(() => getCPUCounter());
+                label4.Text = await Task.Run(() => getRAMCounter());
+                label6.Text = await Task.Run(() => getDriveInfo());
+           }
+        }
+        
+    }
+}
