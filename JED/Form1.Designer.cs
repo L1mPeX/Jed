b7d80d90e3fe5fd1159e787e25502a833af58527
itthingsyt <58787859+itthingsyt@users.noreply.github.com>
2022-07-13 22:29:10 +0300

    -Оптимизация кода и повышение эфективности работы программы -Добавлены комментарии

--- JED/Form1.Designer.cs
+++ JED/Form1.Designer.cs
@@ -28,7 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jed));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -36,9 +43,20 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -80,6 +98,7 @@
             this.label3.Size = new System.Drawing.Size(146, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Оперативная память (ОЗУ)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
@@ -93,19 +112,46 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(21, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Центральный процессор (ЦП)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Location = new System.Drawing.Point(4, 3);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(624, 102);
             this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(130, 14);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // panel3
             // 
@@ -118,15 +164,63 @@
             // panel4
             // 
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.chart2);
+            this.panel4.Controls.Add(this.chart1);
             this.panel4.Location = new System.Drawing.Point(239, 111);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(559, 337);
+            this.panel4.Size = new System.Drawing.Size(558, 337);
             this.panel4.TabIndex = 3;
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(-1, -1);
+            this.chart2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "RAM";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(558, 337);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            this.chart2.Visible = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(-1, -1);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            series2.Legend = "Legend1";
+            series2.Name = "CPU";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(558, 336);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
             // 
             // Jed
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(803, 453);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
@@ -141,6 +235,12 @@
             this.Load += new System.EventHandler(this.Jed_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,6 +256,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
 
--- JED/Form1.cs
+++ JED/Form1.cs
@@ -3,35 +3,105 @@ using System.IO;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace JED
 {
     public partial class Jed : Form
     {
-        
-        public Jed()
+        private double[] cpuArray = new double[60]; // массив со значениями нагрузки проца. передаётся для визуализации данных графика
+        private double[] ramArray = new double[60]; // массив со значениями занятой ОЗУ. передаётся для визуализации данных графика
+        private void getPerformanceCounters() // метод для получения загрузки проца и передачи данных в массив для визуализации в графике
+        {
+            var cpuPerfCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total"); /* PerformanceCounter - счётчик всяких данных.
+                                                                                                                 * через него можно посмотреть загрузку проца и инфы по дискам,
+                                                                                                                 * но озу нельзя почему-то*/
+
+            while (true)
+            {
+                cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(), 0); // заносим значения в массив, обрабатываем
+
+                Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+
+                if (chart1.IsHandleCreated) /* это условие проверяет, создан ли какой-либо элемент управления(присвоен ли ему свой базовый декриптор),
+                                             * если да, то выполняет действие(делегат) в отдельном потоке
+                                             это чудо позволяет каждое отдельное взаимодействие с элементом интерфейса программы выполнять по отдельности
+                                             и не захломлять один поток. Пример: отрисовка формы происходит в одном потоке, а изменение текста в другом
+                                            (благодаря этому подходу прога не лагает каждую секунду, когда мы делаем thread(time) sleep и всё летает по красоте*/
+                        
+
+                {
+                    this.Invoke((MethodInvoker) delegate { UpdateCpuChart(); });
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void UpdateCpuChart() // метод, отвечающий за добавление данных по процу в график. 
+        {
+            chart1.ChartAreas[0].AxisY.Maximum = 100; // y наиб = 100%
+            chart1.Series["CPU"].Points.Clear(); // очищаем нанесённые точки(чтобы график был непрерывным)
+
+            for (int i = 0; i < cpuArray.Length - 1; ++i)
+            {
+                chart1.Series["CPU"].Points.AddY(cpuArray[i]); // отмечаем точки
+            }
+        }
+
+        private void getRAM() // метод для получения озу и передачи данных в массив для визуализации в графике
+        {
+            double A_RAM = Math.Round(new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / 1073741824.0, 2); // сколько свободно ОЗУ на ПК
+            double T_RAM = ((new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1073741824) + 1); // сколько доступно физически ОЗУ на ПК
+            double F_RAM = T_RAM - A_RAM; // сколько занято ОЗУ на ПК
+
+            while (true)
+            {
+                ramArray[ramArray.Length - 1] = F_RAM; // заносим данные(занятое ОЗУ) в массив и обрабатываем по аналогии с процом. Также добавляем асинхронность 
+
+                Array.Copy(ramArray, 1, ramArray, 0, ramArray.Length - 1);
+
+                if (chart2.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateRAMChart(); });
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        private void UpdateRAMChart() // метод, отвечающий за добавление данных по ОЗУ в график. 
+        {
+            chart2.ChartAreas[0].AxisY.Maximum = (int)((new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1073741824) + 1); // у наиб = сколько доступно физически ОЗУ
+            chart2.Series["RAM"].Points.Clear(); // очищаем нанесённые точки(чтобы график был непрерывным)
+
+            for (int i = 0; i < ramArray.Length - 1; ++i)
+            {
+                chart2.Series["RAM"].Points.AddY(ramArray[i]); // отмечаем точки
+            }
+        }
+
+
+        private void label1_Click(object Sender, EventArgs e) // когда жмём на текст с нагрузкой проца, включается график с ЦП и выключается график с ОЗУ
+        {
+            chart1.Visible = true;
+            chart2.Visible = false;
+        }
+        public Jed() // самый главный класс формы(нашей проги). инициализируем всё и запускаем отдельные потоки с процессом сбора данных по процу и ОЗУ
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen; // прога при запуске встаёт ровно по центру экрана
+            Thread cpuThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+            Thread cpuRAMThread = new Thread(new ThreadStart(this.getRAM));
+            cpuRAMThread.IsBackground = true;
+            cpuRAMThread.Start();
         }
-          
-        public string getCPUCounter()
-        {
-            PerformanceCounter cpuCounter = new PerformanceCounter();
-            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            dynamic firstValue = cpuCounter.NextValue();
-            Thread.Sleep(1000); 
-            dynamic secondValue = cpuCounter.NextValue();
-            return "Нагрузка: " + (int)secondValue + "%";
-
-        }
-        public string getRAMCounter()
+               
+        private string getRAMCounter() // метод для передачи инфы по ОЗУ для текстового поля
         {
             double A_RAM = Math.Round(new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory / 1073741824.0, 2);
             ulong T_RAM = ((new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1073741824) + 1);
             return "Используется: " + (T_RAM - A_RAM) + "ГБ / " + T_RAM + "ГБ";
         }
-        public string getDriveInfo()
+        private string getDriveInfo() // метод для передачи инфы по дискам и памяти для текстового поля
         {
             var drives = DriveInfo.GetDrives();
             string fin_res = "";
@@ -42,19 +112,51 @@ namespace JED
             return fin_res;
         }
 
-        private async void Jed_Load(object sender, EventArgs e)
+        private void PC_DATA() // объединяющий метод, который обновляет текстовую инфу
         {
-            await PC_DATA();
+            for (; ; )
+            {
+                if (label2.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        for (int i = 0; i < cpuArray.Length - 1; ++i)
+                        {
+                            label2.Text = "Нагрузка: " + (int)cpuArray[i] + "%";
+                        }
+                    });
+
+                    if (label4.IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate { label4.Text = getRAMCounter(); });
+                    }
+                    if (label6.IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate { label6.Text = getDriveInfo(); });
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
         }
-        private async Task PC_DATA()
+
+        private void pictureBox2_Click(object sender, EventArgs e) // когда жмём на плюсик. временная заглушка
         {
-           for (; ; )
-           {
-                label2.Text = await Task.Run(() => getCPUCounter());
-                label4.Text = await Task.Run(() => getRAMCounter());
-                label6.Text = await Task.Run(() => getDriveInfo());
-           }
+            MessageBox.Show("В данный момент недоступно");
         }
-        
+
+        private void Jed_Load(object sender, EventArgs e) /* метод, который срабатывает после полной загрузки и отрисовки формы(проги). тут мы 
+                                                           * запускаем наш "объединяющий метод" */
+        {
+            Thread GET_INFO = new Thread(new ThreadStart(this.PC_DATA));
+            GET_INFO.IsBackground = true;
+            GET_INFO.Start();
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+            chart2.Visible = true;
+        }
     }
 }
